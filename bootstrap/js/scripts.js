f3b5d6dd695e4ad4c2566db3f0962460c5533877
jamielynnroth <roth.j@me.com>
2014-02-03 10:06:51 -0500

    first commit

---
+++ bootstrap/js/scripts.js
@@ -0,0 +1,6 @@
+// $('nav ul li #aboutus').click(function () {
+// 	$('#popup-background').removeClass('invisible');
+// 	$('#about-popup').removeClass('invisible');
+// });
+
+$('.modal').hasClass('in').css('display','block');
